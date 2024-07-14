d605800e9f9ab10c608cdc1e72334739c4b722d8
Raul Marquez <85851205+Echo5Mike@users.noreply.github.com>
2024-07-14 08:25:58 -0700

    updated errors

--- js2/js-higher-order-function.js
+++ js2/js-higher-order-function.js
@@ -1,6 +1,6 @@
 // Create a function that takes in an array of numbers and returns an array with all numbers multiplied by 10.
 
-const arr1 = [3, 9, 15, 4, 10]
+const arr1 = [3, 9, 15, 4, 10];
 
 // ----------------Good for muiltiple arrays----------------------
 // const numArray = (numsArray) => {
@@ -17,11 +17,11 @@ const arr1 = [3, 9, 15, 4, 10]
 
 // ----------------One liner----------------------
 
-const numArray3 = arr1.map((arrayValue) => arrayValue * 10)
+const numArray3 = arr1.map((arrayValue) => arrayValue * 10);
 
 // console.log(numArray(arr1))
 // console.log(numArray2)
-console.log(numArray3)
+console.log(numArray3);
 // output: [30, 90, 150, 40, 100]
 
 // --------------------------------------------------------------------------------
@@ -41,19 +41,19 @@ console.log(numArray3)
 // ---------------------- V V V V  Function and one liner V V V -----------------
 
 const divBy2 = (numArray) => {
-  return numArray.map((value) => value / 2)
-}
+  return numArray.map((value) => value / 2);
+};
 
 // ------------------------vvvvvvvvv one liner vvvvvvvvv ----------------------
 // const multiArrayX2 = arr1.map((arrayValue) => arrayValue * 2)
-console.log(divBy2(arr1))
+console.log(divBy2(arr1));
 // output: [1.5, 4.5, 7.5, 2, 5]
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
 // Create a function that takes in an array of numbers and returns an array with only odd numbers.
-const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
+const arr2 = [2, 7, 3, 5, 8, 10, 13, -9];
 
 // const onlyOdds = (array) => {
 //   return array.filter((value) => {
@@ -63,9 +63,9 @@ const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
 
 // ---------- Top function and bottom function do the same task -----------------
 
-const onlyOdds = (numArray) => numArray.filter((value) => value % 2 != 0)
+const onlyOdds = (numArray) => numArray.filter((value) => value % 2 != 0);
 
-console.log(onlyOdds(arr2))
+console.log(onlyOdds(arr2));
 // output: [7, 3, 5, 13, -9]
 
 // --------------------------------------------------------------------------------
@@ -73,16 +73,16 @@ console.log(onlyOdds(arr2))
 
 // Create a function that takes in a string of multiple words and returns an array with only the words that have an odd number of characters.
 
-const pumbaa = "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze"
+const pumbaa = "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze";
 
 const hakuna = (string) => {
   // let temp = string.split(" ")
   // return temp.filter((value) => value.length % 2 !== 0)
 
-  return string.split(" ").filter((value) => value.length % 2 !== 0)
-}
+  return string.split(" ").filter((value) => value.length % 2 !== 0);
+};
 
-console.log(hakuna(pumbaa))
+console.log(hakuna(pumbaa));
 
 // output: ["a", "wonderful", "ain't", "passing", "craze"]
 
@@ -90,22 +90,22 @@ console.log(hakuna(pumbaa))
 // --------------------------------------------------------------------------------
 
 // Create a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
-const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"];
 
 // const returnStrings = (array) => {
 //   let temp = array.filter((value) => typeof value === "string")
 //   return temp.join("")
 // }
 
-const returnStrings = (array) => array.filter((value) => typeof value === "string").join("")
-console.log(returnStrings(comboArr))
+const returnStrings = (array) => array.filter((value) => typeof value === "string").join("");
+console.log(returnStrings(comboArr));
 // output: "nicework"
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-const filterArrayValues = [58, "", "abcd", true, null, false, 0]
+const filterArrayValues = [58, "", "abcd", true, null, false, 0];
 
 // const noFalseValues = (array) => {
 //   let result = array.filter((value) => {
@@ -114,45 +114,45 @@ const filterArrayValues = [58, "", "abcd", true, null, false, 0]
 //   return result
 // }
 
-const noFalseValues = (array) => array.filter((value) => value !== null && value !== undefined && value !== 0 && value !== false && value !== "")
+const noFalseValues = (array) => array.filter((value) => value !== null && value !== undefined && value !== 0 && value !== false && value !== "");
 
-console.log(noFalseValues(filterArrayValues))
+console.log(noFalseValues(filterArrayValues));
 
 // output: [58, "abcd", true]
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
-// Create a function that takes in an array of strings and returns an maiarray of strings with every other letter capitalized.
+// Create a function that takes in an array of strings and returns an array of strings with every other letter capitalized.
 
-const makesWackyWords = ["simba", "nala", "zazu", "rafiki"]
+const makesWackyWords = ["simba", "nala", "zazu", "rafiki"];
 
 const secondLetterCap = (array) => {
   let temp = array.map((value) => {
-    let result = value.split("").join("")
-    let result2 = []
+    let result = value.split("").join("");
+    let result2 = [];
 
     for (let i = 0; i < value.length; i++) {
       if (i % 2 !== 0) {
-        result2.push(result[i].toUpperCase())
+        result2.push(result[i].toUpperCase());
       } else {
-        result2.push(result[i])
+        result2.push(result[i]);
       }
     }
 
-    return result2.join("")
-  })
-  return temp
-}
+    return result2.join("");
+  });
+  return temp;
+};
 
-console.log(secondLetterCap(makesWackyWords))
+console.log(secondLetterCap(makesWackyWords));
 // output: ["sImBa", "nAlA", "zAzU", "rAfIkI"]
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-const str = "javascript is awesome"
+const str = "javascript is awesome";
 
 // const voweFilter = (str) => {
 //   let strArray = str.split("")
@@ -164,9 +164,9 @@ const str = "javascript is awesome"
 //   return result.join("")
 // }
 
-const voweFilter = (str) => str.split("").filter((value) => value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u")
+const voweFilter = (str) => str.split("").filter((value) => value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u");
 
-console.log(voweFilter(str))
+console.log(voweFilter(str));
 // output: "jvscrpt s wsm"
 
 // --------------------------------------------------------------------------------
@@ -174,11 +174,11 @@ console.log(voweFilter(str))
 
 // Create a function that takes in a string containing numbers and returns an array with each number at its own index and converted to the data type of number.
 
-const stringOfNumbers = "4574328"
+const stringOfNumbers = "4574328";
 
-const strNums = (str) => str.split("").map((value) => Number(value))
+const strNums = (str) => str.split("").map((value) => Number(value));
 
-console.log(strNums(stringOfNumbers))
+console.log(strNums(stringOfNumbers));
 // output: [4, 5, 7, 4, 3, 2, 8]
 
 // --------------------------------------------------------------------------------
@@ -186,21 +186,21 @@ console.log(strNums(stringOfNumbers))
 
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 
-const arrNums3 = [3, 7, 10, 5, 4, 3]
-const arrNums4 = [7, 8, 2, 1, 5, 4]
+const arrNums3 = [3, 7, 10, 5, 4, 3];
+const arrNums4 = [7, 8, 2, 1, 5, 4];
 
-const noDubs = (numArray1, numArray2) => [...new Set([...numArray1, ...numArray2])]
+const noDubs = (numArray1, numArray2) => [...new Set([...numArray1, ...numArray2])];
 
-console.log(noDubs(arrNums3, arrNums4))
+console.log(noDubs(arrNums3, arrNums4));
 // output: [3, 7, 10, 5, 4, 8, 2, 1]
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
 // Create a function that takes in an array of mixed data types and returns the first value that is a string. HINT: look into the JavaScript .find() method.
-const allTheData = [7, null, true, 2, "yo!", false, 4, "hello!"]
+const allTheData = [7, null, true, 2, "yo!", false, 4, "hello!"];
 
-const firstString = (arr) => arr.find((element) => typeof element === "string")
+const firstString = (arr) => arr.find((element) => typeof element === "string");
 
-console.log(firstString(allTheData))
+console.log(firstString(allTheData));
 // output: "yo!"
